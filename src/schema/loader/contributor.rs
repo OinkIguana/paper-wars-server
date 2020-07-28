f2273e7787c4fd7520e638edbcbd43d772c85571
Cameron Eldridge <cameldridge@gmail.com>
2020-07-27 23:28:40 -0400

    Add more macros to reduce code duplication

--- src/schema/loader/contributor.rs
+++ src/schema/loader/contributor.rs
@@ -1,35 +1,10 @@
 use super::Loader;
-use data::{contributors, Contributor};
-use diesel::prelude::*;
-use tokio::task;
+use data::Contributor;
 use uuid::Uuid;
 
 batch_fn!(contributors => Contributor { universe_id: Uuid, account_id: Uuid });
 
 impl Loader<(Uuid, Uuid), Contributor> {
-    pub async fn for_account(&self, id: &Uuid) -> Vec<Contributor> {
-        let load_result: anyhow::Result<Vec<Contributor>> = task::block_in_place(|| {
-            let conn = self.database.connection()?;
-            Ok(contributors::table
-                .filter(contributors::account_id.eq(id))
-                .load(&conn)?)
-        });
-
-        let items = load_result.unwrap_or(vec![]);
-        self.prime_many(items.clone()).await;
-        items
-    }
-
-    pub async fn for_universe(&self, id: &Uuid) -> Vec<Contributor> {
-        let load_result: anyhow::Result<Vec<Contributor>> = task::block_in_place(|| {
-            let conn = self.database.connection()?;
-            Ok(contributors::table
-                .filter(contributors::universe_id.eq(id))
-                .load(&conn)?)
-        });
-
-        let items = load_result.unwrap_or(vec![]);
-        self.prime_many(items.clone()).await;
-        items
-    }
+    join!(contributors => for_account(account_id: Uuid) -> Contributor);
+    join!(contributors => for_universe(universe_id: Uuid) -> Contributor);
 }
--- src/schema/loader/email.rs
+++ src/schema/loader/email.rs
@@ -1,23 +1,10 @@
 use super::Loader;
-use data::{emails, Email};
-use diesel::prelude::*;
+use data::Email;
 use diesel_citext::types::CiString;
-use tokio::task;
 use uuid::Uuid;
 
 batch_fn!(emails => Email { address: CiString });
 
 impl Loader<CiString, Email> {
-    pub async fn for_account(&self, id: &Uuid) -> Vec<Email> {
-        let load_result: anyhow::Result<Vec<Email>> = task::block_in_place(|| {
-            let conn = self.database.connection()?;
-            Ok(emails::table
-                .filter(emails::account_id.eq(id))
-                .load(&conn)?)
-        });
-
-        let items = load_result.unwrap_or(vec![]);
-        self.prime_many(items.clone()).await;
-        items
-    }
+    join!(emails => for_account(account_id: Uuid) -> Email);
 }
